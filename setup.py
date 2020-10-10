9da609f02b2f89fc19065f6388d106aa20e594a8
weiT1993 <tangwei1027@gmail.com>
2020-10-10 15:32:16 -0400

    try pip package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='helper_functions',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/weiT1993/helper_functions.git',
+      author='Wei Tang',
+      author_email='tangwei1027@gmail.com',
+      license='MIT',
+      packages=['helper_functions'],
+      zip_safe=False)
