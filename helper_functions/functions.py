7ad82915d05ca8df8cf99259fa19b6a0dab906cc
weiT1993 <tangwei1027@gmail.com>
2020-10-10 15:59:08 -0400

    changes

---
+++ helper_functions/functions.py
@@ -0,0 +1,3 @@
+def sum_two_nums(a,b):
+    print('Testing pip, %d+%d=%d'%(a,b,a+b))
+    print('testing symlink install')
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(name='helper_functions',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='https://github.com/weiT1993/helper_functions.git',
-      author='Wei Tang',
-      author_email='tangwei1027@gmail.com',
-      license='MIT',
-      packages=['helper_functions'],
-      zip_safe=False)
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='helper_functions_weiT1993',
+    version='0.0.1',
+    description='The funniest joke in the world',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/weiT1993/helper_functions.git',
+    packages=setuptools.find_packages(),
+    author='weiT1993',
+    author_email='tangwei1027@gmail.com',
+    license='MIT',
+    python_requires='>=3.7',
+    zip_safe=False)
