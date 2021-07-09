7a15457ef8e5e5e381b6bd139590def73fcc60fd
weiT1993 <tangwei1027@gmail.com>
2021-07-09 02:20:29 -0400

    debugged metrics

--- qiskit_helper_functions/metrics.py
+++ qiskit_helper_functions/metrics.py
@@ -1,8 +1,11 @@
 import numpy as np
+import copy
 from sklearn.linear_model import LinearRegression
 from qiskit.quantum_info import Statevector
 
 def chi2_distance(target,obs):
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
     obs = np.absolute(obs)
     if isinstance(target,np.ndarray):
         assert len(target)==len(obs)
@@ -27,6 +30,8 @@ def MSE(target,obs):
     '''
     Mean Square Error
     '''
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
     if isinstance(target,dict):
         se = 0
         for t_idx in target:
@@ -56,23 +61,34 @@ def MAPE(target,obs):
     Mean absolute percentage error
     abs(target-obs)/target
     '''
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
+    epsilon = 1e-16
     if isinstance(target,dict):
+        curr_sum = np.sum(list(target.values()))
+        new_sum = curr_sum + epsilon*len(target)
         mape = 0
         for t_idx in target:
-            t = target[t_idx]
+            t = (target[t_idx]+epsilon)/new_sum
             o = obs[t_idx]
             mape += abs((t-o)/t)
         mape /= len(obs)
     elif isinstance(target,np.ndarray) and isinstance(obs,np.ndarray):
-        target = target.reshape(-1,1)
-        obs = obs.reshape(-1,1)
+        target = target.flatten()
+        target += epsilon
+        target /= np.sum(target)
+        obs = obs.flatten()
+        obs += epsilon
+        obs /= np.sum(obs)
         mape = np.abs((target-obs)/target)
         mape = np.mean(mape)
     elif isinstance(target,np.ndarray) and isinstance(obs,dict):
+        curr_sum = np.sum(list(target.values()))
+        new_sum = curr_sum + epsilon*len(target)
         mape = 0
         for o_idx in obs:
             o = obs[o_idx]
-            t = target[o_idx]
+            t = (target[o_idx]+epsilon)/new_sum
             mape += abs((t-o)/t)
         mape /= len(obs)
     else:
@@ -80,6 +96,8 @@ def MAPE(target,obs):
     return mape*100
 
 def fidelity(target,obs):
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
     if isinstance(target,np.ndarray):
         assert len(target)==len(obs)
         fidelity = 0
@@ -98,6 +116,8 @@ def fidelity(target,obs):
     return fidelity
 
 def cross_entropy(target,obs):
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
     if isinstance(target,dict):
         CE = 0
         for t_idx in target:
@@ -122,12 +142,16 @@ def cross_entropy(target,obs):
         raise Exception('target type : %s, obs type : %s'%(type(target),type(obs)))
 
 def relative_entropy(target,obs):
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
     return cross_entropy(target=target,obs=obs) - cross_entropy(target=target,obs=target)
 
 def correlation(target,obs):
     '''
     Measure the linear correlation between `target` and `obs`
     '''
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
     target = target.reshape(-1, 1)
     obs = obs.reshape(-1, 1)
     reg = LinearRegression()
@@ -139,6 +163,8 @@ def HOP(target,obs):
     '''
     Measures the heavy output probability
     '''
+    target = copy.deepcopy(target)
+    obs = copy.deepcopy(obs)
     target_median = np.median(target)
     hop = 0
     for t,o in zip(target,obs):
